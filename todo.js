e5a4375b5b77daf6d75a5aa6d4ef4ae751577600
mukundhreddy9849 <113026832+mukundhreddy9849@users.noreply.github.com>
2022-11-15 22:11:33 +0530

    Add files via upload

---
+++ todo.js
@@ -0,0 +1,48 @@
+const todoList = () => {
+  let all = [];
+  const add = (todoItem) => {
+    all.push(todoItem);
+  };
+  const markAsComplete = (index) => {
+    all[index].completed = true;
+  };
+
+  const overdue = () => {
+    return all.filter(
+      (data) => data.dueDate < new Date().toLocaleDateString("en-CA")
+    );
+  };
+
+  const dueToday = () => {
+    return all.filter(
+      (data) => data.dueDate === new Date().toLocaleDateString("en-CA")
+    );
+  };
+
+  const dueLater = () => {
+    return all.filter(
+      (data) => data.dueDate > new Date().toLocaleDateString("en-CA")
+    );
+  };
+  const toDisplayableList = (list) => {
+    return list
+      .map(
+        (data) =>
+          `${data.completed ? "[x]" : "[ ]"} ${data.title} ${
+            data.dueDate == today ? "" : data.dueDate
+          }`
+      )
+      .join("\n")
+      .trim();
+  };
+  return {
+    all,
+    add,
+    markAsComplete,
+    overdue,
+    dueToday,
+    dueLater,
+    toDisplayableList,
+  };
+};
+module.exports = todoList;
